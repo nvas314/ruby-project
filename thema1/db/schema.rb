ec7683f750e07f10d91b233271c4cbe9ea2e7873
nvas314 <nikosvas314@gmail.com>
2024-06-02 00:24:12 +0300

    modified:   thema1/app/views/users/index.html.erb 	modified:   thema1/db/schema.rb

--- thema1/db/schema.rb
+++ thema1/db/schema.rb
@@ -10,7 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_01_184736) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_01_211722) do
+  create_table "communities", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "community_posts", force: :cascade do |t|
+    t.string "community_id"
+    t.string "user"
+    t.datetime "timestamp"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "group_messages", force: :cascade do |t|
+    t.integer "group_id"
+    t.string "user_from"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "user_created"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "private_messages", force: :cascade do |t|
+    t.string "user_from"
+    t.string "user_to"
+    t.datetime "timestamp"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "name"
